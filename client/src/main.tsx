f90db453092c3e6677f91d5ad0ce1e0519dd90c7
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-03-10 20:39:21 +0300

    new

---
+++ client/src/main.tsx
@@ -0,0 +1,18 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { lazy, Suspense } from 'react'
+
+
+const Home = lazy(() => import('./pages/home'))
+
+createRoot(document.getElementById('root')!).render(
+  <Suspense fallback={<h1>Loading...</h1>}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </Suspense>
+
+)
