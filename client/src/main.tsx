dd94cf3b305282db7ca16da28cc92ffd5200a289
Tolga BAYRAK <bayraktolga28@gmail.com>
2025-03-10 20:45:45 +0300

    nw

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { lazy, Suspense } from 'react'
+import Loading from './components/loading'
 
 
 const Home = lazy(() => import('./pages/home'))
 
+const NotFoundPage = lazy(() => import('./pages/not-found'))
+
 createRoot(document.getElementById('root')!).render(
-  <Suspense fallback={<h1>Loading...</h1>}>
+  <Suspense fallback={<Loading />}>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   </Suspense>
